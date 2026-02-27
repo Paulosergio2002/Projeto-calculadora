5c2e6dc0918888034283d66036b040aa062f86f3
Paulo Sérgio <paulo.almeidazero01@gmail.com>
2026-02-26 23:09:13 -0400

    Add files via upload

---
+++ project_calculator.py
@@ -0,0 +1,16 @@
+number_1 = float(input('Digite o primeiro número: '))
+number_2 = float(input('Digite o próximo número: '))
+operador = str(input(f'Qual operador você deseja? '))
+if operador == '+':
+    print(number_1 + number_2)
+elif operador == '-':
+    print(number_1 - number_2)
+elif operador == '*':
+    print(number_1 * number_2)
+elif operador == '/':
+    print(number_1 / number_2)
+else:
+    print('Operação Inválida')
+
+
+    
