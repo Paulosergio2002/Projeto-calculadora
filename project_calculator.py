31043101e910f5a25a6ae4abfc4931985df4602b
Paulo Sérgio <paulo.almeidazero01@gmail.com>
2026-02-27 21:12:25 -0400

    modified:   project_calculator.py

--- project_calculator.py
+++ project_calculator.py
@@ -1,18 +1,24 @@
 import time
+def calculadora(number_1, number_2, operador):
+    if operador == '+':
+        return(number_1 + number_2)
+    elif operador == '-':
+        return(number_1 - number_2)
+    elif operador == '*':
+        return(number_1 * number_2)
+    elif operador == '/':
+        if number_2 != 0:
+            return(number_1 / number_2)
+        else:
+            return 'Erro por divisão 0!'
+    else:
+        return('Operação Inválida')
 number_1 = float(input('Digite o primeiro número: '))
 number_2 = float(input('Digite o próximo número: '))
 operador = str(input(f'Qual operador você deseja? '))
-if operador == '+':
-    print(number_1 + number_2)
-elif operador == '-':
-    print(number_1 - number_2)
-elif operador == '*':
-    print(number_1 * number_2)
-elif operador == '/':
-    print(number_1 / number_2)
-else:
-    print('Operação Inválida')
-time.sleep(15)       
+resultado = calculadora(number_1, number_2, operador)
+print(resultado)
+time.sleep(5)       
 
 
     
